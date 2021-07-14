e51e01efd5e791fc320c66c2f1c9ff7eceebc17f
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-14 15:25:41 -0300

    implementa logica de somar os preco total

--- marketplace/src/pages/ShoppingCart.jsx
+++ marketplace/src/pages/ShoppingCart.jsx
@@ -1,7 +1,64 @@
-function ShoppingCart() {
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import propTypes from 'prop-types';
+import { Card, ListGroup } from "react-bootstrap";
+import Header from "../components/Header";
+
+import '../styles/ShoppingCart.css';
+
+function ShoppingCart({itemsInShoppingCart}) {
+
+  function sumTotalValue() {
+    const totalValue = itemsInShoppingCart.reduce((acc, currentValue) => Number(acc) + Number(currentValue.price),[0]);
+
+    return Number(totalValue).toFixed(2);
+  }
+  
+  useEffect(() => {
+    sumTotalValue()
+  }, [itemsInShoppingCart])
+  
   return (
-    <p>carrinho de compras</p>
+    <section className="shopping">
+      <Header />
+        <Card className="shopping__card">
+          <Card.Header>
+            <span className="card__quantity">Itens no carrinho: {itemsInShoppingCart.length}</span>
+            <span className="card__total">
+              Total: {sumTotalValue()}
+            </span>
+          </Card.Header>
+          <ListGroup variant="flush">
+            {
+              itemsInShoppingCart.map((item) => {
+                console.log('oi');
+                return (
+                  <ListGroup.Item key={item.id}>
+                    <img src={item.thumbnail} alt={item.title} width="70px" height="70px"/>
+                    <p className="card__title">{item.title}</p>
+                    <p>Quantidade: 1</p>
+                    <p className="card__price">R$ {item.price.toFixed(2)}</p>
+                  </ListGroup.Item>
+                )
+              })
+            }
+          </ListGroup>
+        </Card>
+    </section>
   )
 }
 
-export default ShoppingCart;
+const mapStateToProps = (state) => ({
+  itemsInShoppingCart: state.market.payload.productsInShoppingCart
+});
+
+
+ShoppingCart.propTypes = {
+  itemsInShoppingCart: propTypes.arrayOf(propTypes.object),
+};
+
+ShoppingCart.defaultProps = {
+  itemsInShoppingCart: []
+};
+
+export default connect(mapStateToProps)(ShoppingCart);
