2ad91b5b0b867832ebd973c5f8ae46bcd11b981d
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-14 10:56:52 -0300

    arruma estilizacao ToastBox

--- marketplace/src/components/ToastBox.jsx
+++ marketplace/src/components/ToastBox.jsx
@@ -1,26 +1,30 @@
 import { Toast } from 'react-bootstrap';
 import propTypes from 'prop-types';
 
+import '../styles/ToastBox.css';
+
 function ToastBox({ show, setShow }) {
 	return (
-		<Toast
-			onClose={() => setShow(false)}
-			show={show}
-			delay={3000}
-			autohide
-			className="toast"
-		>
-			<Toast.Header>
-				<img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-			</Toast.Header>
-			<Toast.Body>
-				<img
-					src="https://img.icons8.com/material-outlined/24/000000/add-shopping-cart.png"
-					alt="shopping cart plus"
-				/>{' '}
-				Produto adicionado ao carrinho!
-			</Toast.Body>
-		</Toast>
+		<section className="toastbox">
+			<Toast
+				onClose={() => setShow(false)}
+				show={show}
+				delay={3000}
+				autohide
+				className="toastbox__toast"
+			>
+				<Toast.Header>
+					<img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+				</Toast.Header>
+				<Toast.Body>
+					<img
+						src="https://img.icons8.com/material-outlined/24/000000/add-shopping-cart.png"
+						alt="shopping cart plus"
+					/>{' '}
+					Produto adicionado ao carrinho!
+				</Toast.Body>
+			</Toast>
+		</section>
 	);
 }
 
